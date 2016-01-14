dd5e1ed21b53c435af2f237bbaf3b923281e12ff
John Hawthorn <john.hawthorn@gmail.com>
2016-01-14 15:41:36 -0800

    Change formatting of order_updater

--- core/app/models/spree/order_updater.rb
+++ core/app/models/spree/order_updater.rb
@@ -74,15 +74,21 @@ module Spree
 
     def update_adjustment_total
       recalculate_adjustments
-      order.adjustment_total = line_items.sum(:adjustment_total) +
-                               shipments.sum(:adjustment_total)  +
-                               adjustments.eligible.sum(:amount)
-      order.included_tax_total = line_items.sum(:included_tax_total) + shipments.sum(:included_tax_total)
-      order.additional_tax_total = line_items.sum(:additional_tax_total) + shipments.sum(:additional_tax_total)
+      order.adjustment_total =
+        line_items.sum(:adjustment_total) +
+        shipments.sum(:adjustment_total)  +
+        adjustments.eligible.sum(:amount)
+      order.included_tax_total =
+        line_items.sum(:included_tax_total) +
+        shipments.sum(:included_tax_total)
+      order.additional_tax_total =
+        line_items.sum(:additional_tax_total) +
+        shipments.sum(:additional_tax_total)
 
-      order.promo_total = line_items.sum(:promo_total) +
-                          shipments.sum(:promo_total) +
-                          adjustments.promotion.eligible.sum(:amount)
+      order.promo_total =
+        line_items.sum(:promo_total) +
+        shipments.sum(:promo_total) +
+        adjustments.promotion.eligible.sum(:amount)
 
       update_order_total
     end
