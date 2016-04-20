b6936c8050811ea4b2344921443e8ae27c408903
John Hawthorn <john@stembolt.com>
2016-04-20 15:56:10 -0700

    Allow options in safe_add_index

--- core/db/migrate/20141217215630_update_product_slug_index.rb
+++ core/db/migrate/20141217215630_update_product_slug_index.rb
@@ -1,4 +1,6 @@
 class UpdateProductSlugIndex < ActiveRecord::Migration
+  include Spree::MigrationHelpers
+
   def change
     safe_remove_index :spree_products, :slug
     safe_add_index :spree_products, :slug, unique: true
--- core/lib/spree/migration_helpers.rb
+++ core/lib/spree/migration_helpers.rb
@@ -4,9 +4,9 @@ module Spree
       remove_index(table, column) if index_exists?(table, column)
     end
 
-    def safe_add_index(table, column)
-      if column_exists?(table, column) && !index_exists?(table, column)
-        add_index(table, column)
+    def safe_add_index(table, column, options={})
+      if column_exists?(table, column) && !index_exists?(table, column, options)
+        add_index(table, column, options)
       end
     end
   end
