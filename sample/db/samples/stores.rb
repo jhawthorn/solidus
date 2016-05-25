d07a1c539be95fd972d8dc817183bef678f50688
John Hawthorn <john@stembolt.com>
2016-05-25 14:54:11 -0700

    Make Store from sample data the default
    Previously the Store created from the sample data was not being set as
    the default store, which caused problems when trying to edit the General
    settings.

--- sample/db/samples/stores.rb
+++ sample/db/samples/stores.rb
@@ -2,5 +2,6 @@ Spree::Store.create!(
   name: "Sample Store",
   code: "sample-store",
   url: "solidus.example.com",
-  mail_from_address: "store@example.com"
+  mail_from_address: "store@example.com",
+  default: true
 )
--- sample/spec/lib/load_sample_spec.rb
+++ sample/spec/lib/load_sample_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe "Load samples" do
-  it "doesn't raise any error" do
+  it "loads data successfully" do
     expect {
       # Seeds are only run for rake test_app so to allow this spec to pass without
       # rerunning rake test_app every time we must load them in if not already.
@@ -12,5 +12,11 @@ describe "Load samples" do
 
       SpreeSample::Engine.load_samples
     }.to output.to_stdout
+
+    # loading sample data is slow, so we want run all expectations together
+    aggregate_failures("sample data") do
+      expect(Spree::Store.count).to eq(1)
+      expect(Spree::Store.first).to eq(be_default)
+    end
   end
 end
