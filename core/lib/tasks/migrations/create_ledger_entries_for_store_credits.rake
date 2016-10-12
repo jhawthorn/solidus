e1bc681d96fc5f65430abe84130d8f98d9e89e9b
Peter Berkenbosch <peter@stembolt.com>
2016-10-12 18:51:06 -0400

    WIP spec added for the rake task, setup needs some work

--- core/lib/tasks/migrations/create_ledger_entries_for_store_credits.rake
+++ core/lib/tasks/migrations/create_ledger_entries_for_store_credits.rake
@@ -5,12 +5,11 @@ namespace :solidus do
         print 'Creating opening ledger entries for store credits...'
         Spree::StoreCredit.find_each do |store_credit|
           current_balance_amount = store_credit.amount_remaining + store_credit.amount_authorized
-          current_balance_amount = 0 if current_balance_amount < 0
           store_credit.store_credit_ledger_entries.create!({
             amount: current_balance_amount
           })
         end
-        puts ' success'
+        puts 'success'
       end
 
       task down: :environment do
---
+++ core/spec/lib/tasks/migrations/create_ledger_entries_for_store_credits_spec.rb
@@ -0,0 +1,71 @@
+require 'spec_helper'
+
+describe 'solidus:migrations:create_ledger_entries_for_store_credits' do
+  include_context(
+    'rake',
+    task_name: 'solidus:migrations:create_ledger_entries_for_store_credits:up',
+    task_path: Spree::Core::Engine.root.join('lib/tasks/migrations/create_ledger_entries_for_store_credits.rake'),
+  )
+
+  let(:store_credit) { create(:store_credit, store_credit_attr) }
+  let(:store_credit_attr) { {} }
+
+  before(:all) do
+    # simulate legacy store_credit without any ledger entries
+    Spree::StoreCreditLedgerEntry.destroy_all
+  end
+
+  context "Store credit without any changes" do
+    let(:store_credit_attr) { { amount: 250 } }
+
+    it 'will create a ledger entry with the right amount' do
+      store_credit
+      expect {
+        task.invoke
+      }.to change { Spree::StoreCreditLedgerEntry.count }.by(1)
+
+      expect(store_credit.ledger_balance).to eql 250
+    end
+  end
+
+  context "Store credit with some used amounts" do
+    let(:store_credit_attr) { { amount: 250, amount_used: 100 } }
+
+    it 'will create a ledger entry with the right amount' do
+      store_credit
+      expect {
+        task.invoke
+      }.to change { Spree::StoreCreditLedgerEntry.count }.by(1)
+
+      expect(store_credit.ledger_balance).to eql 150
+    end
+  end
+
+  context "Store credit with some used amounts and authorised amounts" do
+    let(:store_credit_attr) { { amount: 250, amount_used: 100, amount_authorized: 50 } }
+
+    it 'will create a ledger entry with the right amount' do
+      store_credit
+      expect {
+        task.invoke
+      }.to change { Spree::StoreCreditLedgerEntry.count }.by(1)
+
+      expect(store_credit.ledger_balance).to eql 150
+    end
+  end
+
+  context "Invalidated store credit" do
+    let(:store_credit_attr) { { amount: 250, amount_used: 100 } }
+    let(:invalidation_user) { create(:user) }
+    let(:invalidation_reason) { create(:store_credit_update_reason) }
+
+    it 'will create a ledger entry with the right amount' do
+      store_credit.invalidate(invalidation_reason, invalidation_user)
+      expect {
+        task.invoke
+      }.to change { Spree::StoreCreditLedgerEntry.count }.by(1)
+
+      expect(store_credit.ledger_balance).to eql 0
+    end
+  end
+end
