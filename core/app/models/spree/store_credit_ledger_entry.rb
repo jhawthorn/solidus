9461e34534d5d3630f6803180bf3f48f8324b115
Peter Berkenbosch <peter@stembolt.com>
2016-10-12 16:15:38 -0400

    use the `store_credit#amount_remaining` and add back the `store_credit#amount_authorised` amount to it to create the correct opening balance `store_credit_ledger_entry`

--- core/app/models/spree/store_credit_ledger_entry.rb
+++ core/app/models/spree/store_credit_ledger_entry.rb
@@ -13,8 +13,10 @@ module Spree
       # for store_credit created before the introduction of
       # the ledger entries
       def generate_opening_ledger_entry_for(store_credit)
+        current_balance_amount = store_credit.amount_remaining + store_credit.amount_authorized
+        current_balance_amount = 0 if current_balance_amount < 0
         store_credit.store_credit_ledger_entries.create!({
-          amount: store_credit.amount - store_credit.amound_used
+          amount: current_balance_amount
         })
       end
     end
