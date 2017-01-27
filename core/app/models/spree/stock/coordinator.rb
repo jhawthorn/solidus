45846bdaf123d7141037b29eafd43bfa1899b513
John Hawthorn <john@stembolt.com>
2017-01-27 12:46:32 -0800

    Simplify build_*_packages methods
    Remove the "shipments" argument, which was used as a roundabout way to
    concatenate two lists.

--- core/app/models/spree/stock/coordinator.rb
+++ core/app/models/spree/stock/coordinator.rb
@@ -17,7 +17,7 @@ module Spree
 
       def packages
         packages = build_location_configured_packages
-        packages = build_packages(packages)
+        packages += build_packages
         packages = prioritize_packages(packages)
         packages.each do |package|
           package.shipment = package.to_shipment
@@ -38,16 +38,15 @@ module Spree
       # through the rest of the packing / prioritization, lets just put them
       # in packages we know they should be in and deal with other automatically-
       # handled inventory units otherwise.
-      def build_location_configured_packages(packages = [])
-        order.order_stock_locations.where(shipment_fulfilled: false).group_by(&:stock_location).each do |stock_location, stock_location_configurations|
+      def build_location_configured_packages
+        order.order_stock_locations.where(shipment_fulfilled: false).group_by(&:stock_location).flat_map do |stock_location, stock_location_configurations|
           units = stock_location_configurations.flat_map do |stock_location_configuration|
             unallocated_inventory_units.select { |iu| iu.variant == stock_location_configuration.variant }.take(stock_location_configuration.quantity)
           end
           packer = build_packer(stock_location, units)
-          packages += packer.packages
           @preallocated_inventory_units += units
+          packer.packages
         end
-        packages
       end
 
       # Build packages as per stock location
@@ -59,13 +58,12 @@ module Spree
       # for the given order
       #
       # Returns an array of Package instances
-      def build_packages(packages = [])
-        stock_location_variant_ids.each do |stock_location, variant_ids|
+      def build_packages
+        stock_location_variant_ids.flat_map do |stock_location, variant_ids|
           units_for_location = unallocated_inventory_units.select { |unit| variant_ids.include?(unit.variant_id) }
           packer = build_packer(stock_location, units_for_location)
-          packages += packer.packages
+          packer.packages
         end
-        packages
       end
 
       # This finds the variants we're looking for in each active stock location.
