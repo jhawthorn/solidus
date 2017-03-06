9f4c09e0eb6d52613752deef4691812a01545811
John Hawthorn <john.hawthorn@gmail.com>
2017-03-06 10:56:58 -0800

    Switch to using nprogress for progress

--- backend/app/assets/javascripts/spree/backend/progress.js
+++ backend/app/assets/javascripts/spree/backend/progress.js
@@ -1,9 +1,23 @@
-$(function() {
+//= require 'solidus_admin/nprogress'
+
+(function() {
+  var counter = 0;
+
   $(document).ajaxStart(function() {
-    $("#progress").show();
+    if(counter == 0) {
+      NProgress.start();
+    } else {
+      NProgress.inc();
+    }
+    counter += 1;
   });
 
   $(document).ajaxStop(function() {
-    $("#progress").hide();
+    counter -= 1;
+    if(counter == 0) {
+      NProgress.done();
+    } else {
+      NProgress.inc();
+    }
   });
-});
+})();
