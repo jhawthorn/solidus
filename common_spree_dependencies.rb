c6a726ca2c006f44881c5a50d8ab23da655b9f34
John Hawthorn <john.hawthorn@gmail.com>
2017-10-31 11:44:08 -0700

    wip

--- common_spree_dependencies.rb
+++ common_spree_dependencies.rb
@@ -32,7 +32,7 @@ group :test do
   gem 'with_model'
   gem 'rspec_junit_formatter'
   gem 'rails-controller-testing'
-  gem 'ffaker', require: false
+  gem 'ffaker', require: false, github: 'jhawthorn/ffaker', branch: 'autoload'
   gem 'selenium-webdriver'
 end
 
