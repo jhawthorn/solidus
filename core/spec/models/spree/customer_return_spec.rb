87523e2c3ee6a31b99871bae6834e9f9719f36af
John Hawthorn <john.hawthorn@gmail.com>
2017-11-03 14:41:52 -0700

    warning

--- core/spec/models/spree/customer_return_spec.rb
+++ core/spec/models/spree/customer_return_spec.rb
@@ -207,9 +207,15 @@ RSpec.describe Spree::CustomerReturn, type: :model do
       end
 
       it "should NOT raise an error when no stock item exists in the stock location" do
-        inventory_unit.find_stock_item.destroy
+        inventory_unit.find_stock_item.really_destroy!
         create(:customer_return_without_return_items, return_items: [return_item], stock_location_id: new_stock_location.id)
       end
+
+      it "should NOT raise an error when a soft-deleted stock item exists in the stock location" do
+        inventory_unit.find_stock_item.paranoia_destroy
+        create(:customer_return_without_return_items, return_items: [return_item], stock_location_id: new_stock_location.id)
+      end
+
 
       it "should not update the stock item counts in the original stock location" do
         count_on_hand = inventory_unit.find_stock_item.count_on_hand
