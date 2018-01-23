9afd646c83e0db56f3a445a015be2f4b1728d639
John Hawthorn <john@stembolt.com>
2018-01-22 17:32:20 -0800

    Convert tables to Sortable.js

--- backend/app/assets/javascripts/spree/backend/components/sortable_table.js
+++ backend/app/assets/javascripts/spree/backend/components/sortable_table.js
@@ -1,29 +1,29 @@
-//= require solidus_admin/html.sortable
+//= require solidus_admin/Sortable
 
 Spree.ready(function() {
-  var sortables = sortable(
-    'table.sortable tbody', {
-      items: 'tr:not(.unsortable)',
-      forcePlaceholderSize: true
-    });
+  var sortable_tables = document.querySelectorAll('table.sortable');
 
-  _.each(sortables, function(table) {
-    table.addEventListener('sortupdate', function(e) {
-      var positions = {};
-      _.each(e.detail.newStartList, function(el, index) {
-        var idAttr = el.id;
-        if (idAttr) {
-          var objId = idAttr.split('_').slice(-1);
-          positions['positions['+objId+']'] = index + 1;
-        }
-      });
-
-      Spree.ajax({
-        type: 'POST',
-        dataType: 'script',
-        url: $(table).parent('table').data("sortable-link"),
-        data: positions,
-      });
+  _.each(sortable_tables, function(table) {
+    var url = table.getAttribute('data-sortable-link');
+    var tbody = table.querySelector('tbody');
+    var sortable = Sortable.create(tbody,{
+      handle: ".handle",
+      onEnd: function(e) {
+        var positions = {};
+        _.each(e.to.querySelectorAll('tr'), function(el, index) {
+          var idAttr = el.id;
+          if (idAttr) {
+            var objId = idAttr.split('_').slice(-1);
+            positions['positions['+objId+']'] = index + 1;
+          }
+        });
+        Spree.ajax({
+          type: 'POST',
+          dataType: 'script',
+          url: url,
+          data: positions,
+        });
+      }
     });
   });
 });
