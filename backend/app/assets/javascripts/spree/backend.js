2316de62565ec3a270a24e62e11e0553afcece61
John Hawthorn <john.hawthorn@gmail.com>
2018-01-23 10:47:10 -0800

    Remove jquery-ui/sortable

--- backend/app/assets/javascripts/spree/backend.js
+++ backend/app/assets/javascripts/spree/backend.js
@@ -3,7 +3,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery-ui/datepicker
-//= require jquery-ui/sortable
 //= require jquery-ui/autocomplete
 //= require jquery.sticky-kit.min
 //= require solidus_admin/select2
